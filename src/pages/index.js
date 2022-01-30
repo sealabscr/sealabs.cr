ac7dc73a0a57535e36793875435d630e39b14804
Jeam Vega <jean0798@gmail.com>
2022-01-29 21:18:04 -0600

    feature/SEALABS-6: create files structure

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  const Header = () => {
+    return (
+      <div>
+          Under construction
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
