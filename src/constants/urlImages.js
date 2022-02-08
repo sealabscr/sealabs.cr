42f668acc521ef546528892ef9842fd3f1dc6292
Jeam Vega <jean.vega@sealabs.cr>
2022-02-08 16:50:11 -0600

    feature/SEALABS-28: improve code style

--- src/constants/urlImages.js
+++ src/constants/urlImages.js
@@ -4,7 +4,6 @@ export const URL_LOGO_SEALABS_WHITE = "https://raw.githubusercontent.com/sealabs
 export const URL_IMG_LANDINGPAGE_HEADER = "https://raw.githubusercontent.com/sealabscr/public-assets/main/sealabs.cr/img_ladingpage_header.webp"
 export const URL_IMG_CALLTOACTION = "https://raw.githubusercontent.com/sealabscr/public-assets/main/sealabs.cr/img_callToAction.webp"
 
-
 export const VECTOR_LOGO_REACT = "https://raw.githubusercontent.com/sealabscr/public-assets/main/sealabs.cr/vector_logo_react.png"
 export const VECTOR_LOGO_MAGENTO = "https://raw.githubusercontent.com/sealabscr/public-assets/main/sealabs.cr/vector_logo_magento.png"
 export const VECTOR_LOGO_JS = "https://raw.githubusercontent.com/sealabscr/public-assets/main/sealabs.cr/vector_logo_js.png"
